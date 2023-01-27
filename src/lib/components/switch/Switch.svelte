059a88602101af4d9154ae60cb750ed62a05a699
josephgrabinger <joseph.grabinger@yahoo.de>
2023-01-27 11:54:29 +0100

    add switch component

---
+++ src/lib/components/switch/Switch.svelte
@@ -0,0 +1,89 @@
+<script>
+	export let value = true;
+    export let options = ["on", "off"];
+
+    const uniqueID = Math.floor(Math.random() * 100);
+
+    function handleClick(event){
+        const target = event.target
+
+        const state = target.getAttribute('aria-checked')
+
+        value = state === 'true' ? false : true
+    }
+
+</script>
+
+<div class="s s--slider">
+    <button
+        class="flex items-center justify-between px-5"
+        role="switch"
+        aria-checked={value}
+        aria-labelledby={`switch-${uniqueID}`}
+        on:click={handleClick}>
+            <span>{options[0]}</span>
+            <span>{options[1]}</span>
+    </button>
+</div>
+
+<style>
+    :root {
+        --accent-color: CornflowerBlue;
+        --gray: rgb(133, 118, 138);
+    }
+
+/* Slider Design Option */
+
+    .s--slider {
+        display: flex;
+        align-items: center;
+    }
+
+    .s--slider button {
+        width: 10em;
+        height: 2.6em;
+        position: relative;
+        margin: 0 0 0 0.5em;
+        background: var(--gray);
+        border: none;
+    }
+
+    .s--slider button::before {
+        content: '';
+        position: absolute;
+        width: 5.3em;
+        height: 2.33em;
+        background: #fff;
+        top: 0.13em;
+        right: 2.5em;
+        left: 0.13em;
+        transition: transform 0.3s;
+    }
+
+    .s--slider button[aria-checked='true']{
+        background-color: var(--accent-color)
+    }
+
+    .s--slider button[aria-checked='true']::before{
+        transform: translateX(4.5em);
+        transition: transform 0.3s;
+    }
+
+    .s--slider button:focus {
+        box-shadow: 0 0px 0px 1px var(--accent-color);
+    }
+
+    .s--slider button {
+        border-radius: 2.5em;
+    } 
+
+    .s--slider button::before {
+        border-radius: 2.5em;
+    }
+
+    .s--slider button:focus {
+        box-shadow: 0 0px 8px var(--accent-color);
+        border-radius: 2.5em;
+    }
+
+</style>
--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,24 +1,29 @@
 <script>
     import { convertNameToInitials } from '$lib/utils.js';
+    import Switch from '$lib/components/switch/Switch.svelte'
+    
     export let data;
+
+    let switchValue; // true = posts, false = news
 </script>
 
 <div class="max-w-lg mx-auto lg:max-w-7xl">
-    <div class="flex justify-end">
-        <div class="avatar">{convertNameToInitials(data.userName)}</div>
-        <form method="POST" action="?/logout">
-            <button type="submit">
-                <svg class="mt-4 ml-2 hover:fill-sky-700" 
-                    width="35px" 
-                    height="35px" 
-                    viewBox="0 0 24 24">
-                    <path  d="M16,17V14H9V10H16V7L21,12L16,17M14,2A2,2 0 0,1 16,4V6H14V4H5V20H14V18H16V20A2,2 0 0,1 14,22H5A2,2 0 0,1 3,20V4A2,2 0 0,1 5,2H14Z" />
-                </svg>
-            </button>
-        </form>
-    </div>
-    <div class="border-b border-b-gray-200 pb-6">
-        <h2 class="text-3xl tracking-tight font-semibold text-gray-900 sm:text-4xl">Posts</h2>
+    <div class="flex justify-between border-b border-b-gray-200 pb-2">
+        <h2 class="pt-3 align-bottom text-3xl tracking-tight font-semibold text-gray-900 sm:text-4xl">Posts</h2>
+        <Switch bind:value={switchValue} options={['Posts', 'News']}/>
+        <div class="flex">
+            <div class="avatar">{convertNameToInitials(data.userName)}</div>
+            <form method="POST" action="?/logout">
+                <button type="submit">
+                    <svg class="mt-4 ml-2 hover:fill-sky-700" 
+                        width="35px" 
+                        height="35px" 
+                        viewBox="0 0 24 24">
+                        <path  d="M16,17V14H9V10H16V7L21,12L16,17M14,2A2,2 0 0,1 16,4V6H14V4H5V20H14V18H16V20A2,2 0 0,1 14,22H5A2,2 0 0,1 3,20V4A2,2 0 0,1 5,2H14Z" />
+                    </svg>
+                </button>
+            </form>
+        </div>
     </div>
     <div class="mt-12 grid gap-16 lg:grid-cols-3 lg:gap-x-5 lg:gap-y-12">
         {#each data.posts as post}
